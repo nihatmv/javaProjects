964eb25e25a93fe64ea28e8ece377664c2116e31
Nihat Mammadli <nihatmammadli42@gmail.com>
2023-01-26 18:33:29 +0400

    Java file

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        // Scanner scn = new Scanner (System.in);
+        // int a = scn.nextInt();
+        // int x = 5;
+        // if (x>5) {
+        //     System.out.println(x);
+        // }
+        // System.out.println(a);
+        // System.out.println("Hello World");
+        int x = 5;
+        int y = 1;
+        int z = x - y;
+        System.out.println("x - y = " + z);
+    }    
+}
