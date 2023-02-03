b4d0265b91b0a5e183baa459d85b504716f8d2c4
Nihat Mammadli <nihatmammadli42@gmail.com>
2023-02-03 21:25:56 +0400

    Calculator project

--- Main.java
+++ Main.java
@@ -2,18 +2,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        // Scanner scn = new Scanner (System.in);
-        // int a = scn.nextInt();
-        // int x = 5;
-        // if (x>5) {
-        //     System.out.println(x);
-        // }
-        // System.out.println(a);
-        // System.out.println("Hello World");
-        int x = 5;
-        int y = 1;
-        int z = x - y;
-        System.out.println("x - y = " + z);
-    }    
-}
+    public static void main(String[] args){ 
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the seconds:");
+        double second = scanner.nextInt();
+        double hour = second / 3600;
+        double minute = second / 60;
+        System.out.println(second + " second is equal to " + String.format("%.2f",hour) + " hour");
+        System.out.println(second + " second is equal to " + String.format("%.2f",minute) + " minute");
+
+
+    }
+}   
